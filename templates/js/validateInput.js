4eb7d008064a1c306fec1a8832b765c0b5a24e0b
Majewsky <majewsma@AT-WORK.LOCAL>
2019-05-10 15:57:46 +0200

    optimize duplicate code
    optimize duplicate code

--- templates/js/validateInput.js
+++ templates/js/validateInput.js
@@ -1,3 +1,45 @@
+
+function buildSlide(index, offer){
+  /*let offer_id = "offer_" + localStorage.key(index)*/
+  let offer_id = "offer_" + offer.id;
+  let slide = $('<div>')
+    .appendTo('#preview')
+    .attr("id", offer_id )
+    .append( $('<div>').text(offer.id) )
+    .append( $('<div>').text(offer.title) )
+    .append( $('<div>').text(offer.description) )
+    .append( $('<div>').text(offer.price) )
+    .append( $('<button type="button">')
+                .text('Löschen')
+                .attr("id", 'btnDel_' + offer.id)
+                .click(function(){
+                  console.log("Der this context : \n");
+                  console.dir($(this));
+                  console.log("Der this.parent context : \n");
+                  console.dir($(this).parent());
+                  console.log($(this).parent()[0].id);
+                  /* var key = $(this).parent()[0].id; */
+                  console.log("Die offerid im delete button ist: " + offer.id);
+                  /* localStorage.removeItem(key); */
+                  localStorage.removeItem(offer.id);
+                  $(this).parent().remove();
+
+                })
+    )
+}
+
+function evaluateLastOffer(){
+  var maxValue = 1;
+  for (var i = 0; i < localStorage.length; i++) {
+    let toCompare = localStorage.key(i) * 1;
+    if (maxValue <= toCompare) {
+      maxValue = toCompare;
+    }
+  }
+  console.log("Der maxValue ist : " + maxValue);
+  return maxValue;
+}
+
 // validierung für <input>- und <select>-Elemente
 function validateInput(id, typ, classError){
   console.log('validateInput');
@@ -7,7 +49,7 @@ function validateInput(id, typ, classError){
 
   switch(typ){
     case 'stringAlpha':
-      isValid = /^[a-zA-ZüÜäÄöÖß]{2,}$/.test(value);  // min. 2 characters
+      isValid = /^[a-zA-ZüÜäÄöÖß\s\n]{2,}$/.test(value);  // min. 2 characters
       break;
     case 'stringNum':
       isValid = /^[0-9]{1,}$/.test(value);  // min. 1 number
